9d8afee5e6077a03804e3eceff3e05f27e382e52
Ta25Li <2885921@gmail.com>
2025-01-30 11:32:57 +0200

    CssSelectors
    CssSelectors - 30 jan

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -20,12 +20,38 @@ public class HomeWork {
     public void cssLocators(){
         //by tag name
         WebElement div = wd.findElement(By.tagName("div"));
+        WebElement div1 = wd.findElement(By.cssSelector("div"));
+
         WebElement h1 = wd.findElement(By.tagName("h1"));
+        WebElement h1_1 = wd.findElement(By.cssSelector("h1"));
+
         WebElement a = wd.findElement(By.tagName("a"));
+        WebElement a1 = wd.findElement(By.cssSelector("a"));
+
         WebElement button = wd.findElement(By.tagName("button"));
+        WebElement button1 = wd.findElement(By.cssSelector("button"));
+
         List<WebElement> list = wd.findElements(By.tagName("a"));
+        List<WebElement> list1 = wd.findElements(By.cssSelector("a"));
+
         WebElement input = wd.findElement(By.tagName("input"));
+        WebElement input2 = wd.findElement(By.cssSelector("input"));
+
         WebElement form = wd.findElement(By.tagName("form"));
+        WebElement form1 = wd.findElement(By.cssSelector("form"));
+
+        WebElement body = wd.findElement(By.tagName("body"));
+        WebElement body1 = wd.findElement(By.cssSelector("body"));
+// ====================================================================
+
+        //by class
+
+        //by id
+
+        //by attribute
+
+        //by attribute --> starts & ends & contains
+
 
     }
 }
--- src/test/java/Index.java
+++ src/test/java/Index.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -14,9 +15,17 @@ public class Index {
     @BeforeClass
     public void setUp(){
         wd = new ChromeDriver();
-
-        wd.get("file:///C:/Users/Toma/Downloads/21.index.html");
+        wd.get("file:///D:/Study/QA/Auto/21.index.html");
     }
+
+    @Test
+    public void tableTest(){
+        //Canada
+        WebElement canada = wd.findElement(By.cssSelector("tr:nth-child(3) td:last-child"));
+        Assert.assertEquals(canada.getText(),"Canada");
+    }
+
+
 
     @Test
     public void cssLocators(){
@@ -33,5 +42,57 @@ public class Index {
         WebElement form = wd.findElement(By.tagName("form"));
         WebElement form1 = wd.findElement(By.cssSelector("form"));
 
+        //by class
+        WebElement div = wd.findElement(By.className("container"));
+        WebElement div1 = wd.findElement(By.cssSelector(".container"));  // dot name - means looking for class not just element
+
+        List<WebElement> a_list = wd.findElements(By.className("nav-item"));
+        List<WebElement> a_list1 = wd.findElements(By.cssSelector(".nav-item"));
+
+        WebElement nav = wd.findElement(By.id("nav"));
+        WebElement nav1 = wd.findElement(By.cssSelector("#nav")); // indicate that search by id
+
+        WebElement form2 = wd.findElement(By.id("form1"));
+        WebElement form3 = wd.findElement(By.cssSelector("#form1"));
+
+        //by attribute
+        WebElement inputEmail = wd.findElement(By.cssSelector("[placeholder = 'Type your name']"));
+        //start
+        WebElement inputEmail1 = wd.findElement(By.cssSelector("[placeholder ^= 'Type']")); //starts with
+        //ends
+        WebElement inputEmail2 = wd.findElement(By.cssSelector("[placeholder $= 'name']")); //end word
+        //contains
+        WebElement inputEmail3 = wd.findElement(By.cssSelector("[placeholder *= 'your']"));
+
+        WebElement a2 = wd.findElement(By.cssSelector("[href = '#item3']"));
+
+        //by name
+        WebElement inputS = wd.findElement(By.name("surename"));
+        WebElement inputS1 = wd.findElement(By.cssSelector("[name = 'surename']"));
+
+
+
+        //linkText --old and slow
+    //    WebElement a3 = wd.findElement(By.linkText("Item 1"));
+
+        //partialLinkText -- old and slow
+     //   WebElement a3 = wd.findElement(By.partialLinkText("m 1"));
+
+       /*
+       * - find any elements
+       *div -
+      * # id
+      * .class
+      * [name = 'value'] - attribute
+      * .c1.c2 - find 2 classes
+      * a#id.c1.c2
+        *
+        *  ul li:nth-child(3)   ---найти 3го дочернего li по списку
+        * tr:nth-child(3) td:last-child
+         */
+
+
+
     }
+
 }
